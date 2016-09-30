4e3aedbac67e4e9730604ec4f7cde6b020bb6238
Harut <yes@harutune.name>
2016-09-30 18:09:54 +0300

    Add files via upload

---
+++ router.js
@@ -0,0 +1,399 @@
+/*jslint
+    esversion: 6, -W097, browser: true */
+/* globals jQuery: true, $: true, console */
+
+'use strict';
+
+/**
+ * handlerCallback is  a callback that run when handler is picked up and XHR finished
+ *
+ * @callback handlerCallback
+ * @param {String} url URL that we navigating to
+ * @param {String|Element|jQuery} data Data received from server
+ * @param {Boolean} shouldNavigate If set to False it means this is background load
+ *                  and we do not need to perform some actions (e.g. close Reader)
+ * @param {String} from Where does load request come from: 'popstate' or 'external' call
+ * @param {Object=} state Current state object (came from History API)
+ */
+
+/**
+ * Creates router
+ * @class
+ * @classdesc Set of handlers that can process URL handling. Each handler is
+ * an object.
+ * Router has to distinct cases in behaviour: when we handle 'onpopstate'
+ * history event and when some code delegates it to go to some URL. There
+ * is also one extra variation for second behaviour, when we need to load
+ * data for some URL in background (with DOM changes on success but without
+ * navigating to new URL; it used when Reader page loaded synchronously
+ * to load list column).
+ * Therefor we pass to handler more then URL and received data, but also
+ * `from`, `shouldNavigate` and `state` (for future uses) arguments.
+ * @see handlerCallback documentation for details.
+ */
+export function Router(options) {
+    this.options = options || {};
+    this.handlers = [];
+
+    this.supportsPushState = typeof window.history.pushState === 'function';
+
+    if (history.state && history.state.uid) {
+        this.currentUid = this.uid = history.state.uid;
+    } else {
+        this.currentUid = this.uid = 0;
+    }
+    this.initialPop = true;
+    this.initialURL = window.location.href;
+
+    if ('state' in window.history) {
+        this.initialPop = false;
+    }
+
+
+    $(document).ready(function() {
+        // Binds popstate to handle back and forward browser history movements.
+        $(window).on('popstate.router', this.onPopState.bind(this));
+    }.bind(this));
+}
+
+Router.prototype.onPopState = function(e) {
+    console.log(
+        '[Router]: window onPopstate handler. e.state:', e.originalEvent.state,
+        ', window.history.state:', window.history.state
+    );
+
+    var state = e.originalEvent.state;
+    if (state && state.uid && !this.uid) {
+        this.uid = this.currentUid = state.uid;
+    }
+
+    if (this.skip) {
+        this.skip = false;
+        return;
+    }
+
+    if (this.initialPop && state && this.initialURL === state.url) {
+        return;
+    }
+
+    // In chrome and safari popstate happens on pages load, but there
+    // is no way to filter it out without filtering out 'go-back' actions
+    // to first page. So following code leads to double page load
+    // in Chrome and Safari.
+    if (state) {
+        if (typeof this.options.beforeUnload === 'function') {
+            var promptMessage = this.options.beforeUnload();
+            // XXX synchronous
+            if (promptMessage && !window.confirm(promptMessage)){
+                // revert popstate action
+                if (state && state.uid > this.currentUid) {
+                    history.back();
+                } else {
+                    history.forward();
+                }
+                this.skip = true;
+                return;
+            }
+        }
+
+
+
+        this.currentUid = state.uid;
+        if (state.contentView && state.readerView) {
+            this._load(state.contentView, {shouldNavigate: false, from: 'popstate', state: state});
+        }
+        // $.url().attr('relative') returns unicode, which is misinterpreted by IE11.
+        // Don't use it.
+        var l = window.location;
+        var url = l.pathname + l.search + l.hash;
+        this._load(url, {shouldNavigate: false, from: 'popstate', state: state});
+    }
+
+    this.initialPop = false;
+};
+
+Router.prototype.start = function() {
+    //console.debug('[Router]#start');
+    if (!history.state) {
+        var state = {
+            contentView: this.viewManager.getView('contentView').currentURL,
+            readerView: this.viewManager.getView('readerView').currentURL
+        };
+        var url = $.url().attr('relative');
+        this.updateState(state, url, document.title);
+    }
+    this._route($.url().attr('relative'), $('html').html(), {from: 'initial'});
+};
+
+/**
+ * Adds handler to the handler list.
+ * Handler description
+ */
+Router.prototype.map = function(handler) {
+    this.handlers.push(handler);
+};
+
+/**
+ * Changes browser URL and that's it.
+ * XXX: Bad naming since in Backbone.js and durandal.js it used in other way, as we use .load().
+ */
+Router.prototype.navigate = function(state, url, title) {
+    console.log('[Router]: old state', url, state);
+    console.log('[Router]: now will navigate to', url, state);
+    if ('pushState' in window.history) {
+        this.currentUid = state.uid = ++this.uid;
+        window.history.pushState(state, title, url);
+    }
+    //this.trigger('navigated'); // XXX not used
+};
+
+/**
+ * Proxy for history.replaceState.
+ * XXX: Bad naming.
+ */
+Router.prototype.updateState = function(state, url, title) {
+    url = url.replace(/\/+/g, '/');
+    console.log('[Router]: replaceState', url, state);
+    if ('replaceState' in window.history) {
+        state.uid = history.uid;
+        window.history.replaceState(state, title, url);
+    }
+    //this.trigger('replaceState'); // XXX not used
+};
+
+/**
+ * Iterates through all handlers and tests them.
+ * @param {String} url
+ * @param {Object} data
+ * @param {Object} options
+ * @private
+ */
+Router.prototype._route = function(url, data, options, view) {
+    var state,
+        handler;
+    options = $.extend({shouldNavigate: true}, options);
+    // In case there is no handler, we have to navigate to passed URL
+    //if (this.handlers.length === 0 && window.location.href !== url) {
+    //    window.location.href = url;
+    //}
+
+
+    this.viewManager.onRoute(url, data, options.from, options.state, options);
+
+    view = view || this.viewManager.urlToView(url);
+    if (typeof view.onRoute === 'function') {
+        view.onRoute(url, data, options.from, options.state, options);
+    }
+};
+
+//Router.prototype._urlServerPart = function(url) {
+//    if (typeof url === 'string') { url = $.url(url) };
+//    return url.attr('relative').split('#')[0];
+//}
+
+/**
+ * Create and trigger XHR request for passed URL.
+ * @param {String} url
+ * @param {Object} options
+ */
+Router.prototype._load = function(url, options) {
+    //var url = $.url(url).attr('relative');
+    var view = this.viewManager.urlToView(url);
+    //console.log('[Router] route to view', view, 'url', url);
+
+    if (!view.reloadIsRequired(url, options) && !options.force) {
+        console.log('[Router] without reload', url, view.constructor.name);
+
+        this._route(url, null, options, view);
+        this.trigger('loaded', url, {}, options); // XXX the interface differs here and in _load!
+        if (options && options.onLoaded) { options.onLoaded.call(this, url, {}, options); }
+        return;
+    }
+
+    // We should stop previous loading if new request accepted
+    if (this._xhr) {
+        if (options.state && 
+            options.state.contentView == this._xhr.url &&
+            options.state.readerView == url) {
+            //  _load is called for both contentView and readerView!
+            //  this is workaround for that case:
+            //  when contentView generates an xhr,
+            //  it should not be cancelled by readerView
+        } else if (this._xhr.url !== url) {
+            this.abort();
+        } else if (this._xhr.state() === 'pending') {
+            return;
+        }
+    }
+
+    //console.log('[Router]#_load', url, options);
+    this.trigger('willLoad', url); // used to highlight links
+
+    // XXX _load is called for both contentView and readerView!
+    //     these lines cause a bug for sure, but it is not caught yet
+    if (options.state) {
+        options.state.url = url;
+    }
+
+    var data = options.data || [];
+    if (! (data instanceof Array)) {
+        throw 'options.data must be an array';
+    }
+    if (history.state && history.state.contentView) {
+        // Include referer into a page loaded from search
+        // Used to collect stats
+        var referer = history.state.contentView;
+        if (referer.split('?')[0] == '/search') {
+            data = Array.prototype.slice.call(data);
+            data.push({name: 'utm_contentview',
+                       value: referer.split('#')[0]});
+        }
+    }
+
+    this._xhr = $.get(url, data)
+        .done(function(data, textStatus, jqXHR) {
+            this._xhr = null;
+            //console.log('[Router]#_load loaded', url, options);
+            this._route(url, data, options, view);
+            this.trigger('loaded', [url, data, options]);
+            if (options && options.onLoaded) { options.onLoaded.call(this, url, {}, options); }
+        }.bind(this))
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            this._xhr = null;
+            if (textStatus !== 'abort') {
+                this.trigger('failed', {url: url});
+                if (options && options.onFailed) { options.onFailed.call(this, {url: url}); }
+            }
+        }.bind(this));
+    this._xhr.url = url;
+    this._xhr.options = options;
+};
+
+Router.prototype.abort = function() {
+    if (this._xhr) {
+        var options = this._xhr.options;
+        // easy way to define same handler for abort and failed
+        var onAbort = options && options.onAbort;
+        onAbort = (onAbort === 'onFailed') ? options.onFailed : onAbort;
+        var url = this._xhr.url;
+
+        this._xhr.abort();
+
+        this.trigger('abort', {url: url});
+        if (onAbort) { onAbort.call(this, options); }
+    }
+};
+
+/**
+ * Load some URL.
+ * @param {String} url URL to load using AJAX.
+ * @param {Object=} options
+ * @param {Boolean=} options.shouldNavigate Whether we should change URL in location or not.
+ *      This is useful when we need refres content for URL withoud pushing URL to history.
+ *      It's equivalent to `load in background and render but do not change state`.
+ */
+Router.prototype.handleURL = function(url, options) {
+    if (!this.supportsPushState) {
+        if (options && options.from == 'initial') {
+            // Handle left column loading in browsers that do not support
+            // History API.
+            // Otherwise the left column is just closed
+            this._load(url, options);
+            return;
+        }
+        this._fallback($.extend({url: url}, options));
+        return;
+    }
+    if (!options) {
+        options = {data: []};
+    }
+
+    var _options = $.extend({}, {from: 'external'}, options);
+    this._load(url, _options);
+};
+
+/**
+ * Adds event listener to an instance.
+ * @param {(string|Object.<string,*>)} events
+ * @param {*=} selector
+ * @param {*=} data
+ * @param {function(!jQuery.event=)=} handler
+ */
+Router.prototype.on = function(events, selector, data, handler) {
+    $(this).on(events, selector, data, handler);
+};
+
+/**
+ * Adds event listener that will fire just once.
+ * @param {(string|Object.<string,*>)} events
+ * @param {*=} selector
+ * @param {*=} data
+ * @param {function(!jQuery.event=)=} handler
+ */
+Router.prototype.one = function(events, selector, data, handler) {
+    $(this).one(events, selector, data, handler);
+};
+
+/**
+ * Removes event listener.
+ * @param {(string|Object.<string,*>)} events
+ * @param {*=} selector
+ * @param {*=} data
+ * @param {function(!jQuery.event=)=} handler
+ */
+Router.prototype.off = function(events, selector, data, handler) {
+    $(this).off(events, selector, data, handler);
+};
+
+/**
+ * Fires event on an instance.
+ * @param {String} events
+ * @param {Object} params
+ */
+Router.prototype.trigger = function(events, params) {
+    $(this).trigger(events, params);
+};
+
+Router.prototype._fallback = function(options) {
+    var url = $.isFunction(options.url) ? options.url() : options.url,
+        method = options.type ? options.type.toUpperCase() : 'GET';
+
+    var form = $('<form>', {
+        method: method === 'GET' ? 'GET' : 'POST',
+        action: url,
+        style: 'display:none'
+    });
+
+    if (method !== 'GET' && method !== 'POST') {
+        form.append($('<input>', {
+            type: 'hidden',
+            name: '_method',
+            value: method.toLowerCase()
+        }));
+    }
+
+    var data = options.data;
+
+    if (typeof data === 'string') {
+        $.each(data.split('&'), function(index, value) {
+            var pair = value.split('=');
+            form.append($('<input>', {type: 'hidden', name: pair[0], value: pair[1]}));
+        });
+    } else if ($.isArray(data)) {
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].value) {
+                form.append($('<input>', $.extend({type: 'hidden'}, data[i])));
+            }
+        }
+    } else if (typeof data === 'object') {
+        for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+                form.append($('<input>', {type: 'hidden', name: key, value: data[key]}));
+            }
+        }
+    }
+    form.append('<input type="submit" value="Submit" />');
+    $(document.body).append(form);
+    form.submit();
+};
+
---
+++ view.js
@@ -0,0 +1,286 @@
+/*jslint
+    esversion: 6, -W097, browser: true */
+/* globals jQuery:true, $:true, console:true */
+
+'use strict';
+
+/**
+ * Initialize View class. To start work, view have to be attached to DOM with .attachToDOM()
+ * @class View
+ * @classdesc Represent one view. Triggers events: onNavigate, onRender, onBeforeClean
+ * Can be customised in options with onInit and onRender callbacks
+ * @param {Object} options
+ * @param {String} options.url
+ * @param {Element} options.container Element for scrolling view content (like window)
+ * @param {Element=} options.element Element for View
+ * @param {Function} options.onRender
+ * @param {Function} options.onInit Method to be called on View init, so you can tune the view.
+ * @param {Function} options.isEmpty Hacky way to replace .isEmpty() method (since it required sometimes)
+ */
+export function View(options) {
+    this.currentURL = options.url;
+    if (options.onRender) {
+        this.onRender = options.onRender;
+    }
+    if (options.onInit) {
+        this.onInit = options.onInit;
+    }
+    if (options.onShow) {
+        this.onShow = options.onShow;
+    }
+    if (options.onHide) {
+        this.onHide = options.onHide;
+    }
+    if (options.isEmpty && typeof options.isEmpty === 'function') {
+        this.isEmpty = options.isEmpty;
+    }
+    this.container = options.container;
+    this.element = options.element;
+    this.isActive = false;
+    this.isLoading = false;
+    this.state = {};
+}
+
+/**
+ * Attach view to DOM (kind of INIT method, that can be called separately
+ * from view instantiation).
+ * @param {Element|String=} container Element (or selector) that should be used as View body
+ * @param {Element|String=} element
+ */
+View.prototype.attachToDOM = function viewAttachToDOMPrototype(container, element) {
+    if (this.$element) {
+        // We should skip double initialization.
+        return false;
+    }
+    /**
+     * @type {!jQuery|*|jQuery|HTMLElement}
+     */
+    this.$element = $(element ? element : this.element);
+    this.$container = $(container ? container : this.container);
+    if (!this.isEmpty() && !this.currentURL) {
+        var $url = $.url(window.location.href);
+        var fragment = $url.attr('fragment');
+        var query = $url.attr('query');
+        // Since we do not need HASH in view URL state.
+        // XXX what does this comment mean? We do NOT need it, but we add
+        //     it?
+        this.currentURL = $url.attr('path') + (query ? '?' + query : '') +
+                                              (fragment ? '#' + fragment : '');
+    }
+    if (this.bindEvents && typeof this.bindEvents === 'function') {
+        this.bindEvents();
+    }
+    if (this.init && typeof this.init === 'function') {
+        this.init();
+    }
+    if (this.onInit && typeof this.onInit === 'function') {
+        this.onInit();
+    }
+    this.$element.data('view', this);
+    this.trigger('init');
+    return this;
+};
+
+/**
+ * Returns true if View is empty
+ * TODO: Do not work, since sometimes (Reader) container could have elements. FixIt
+ */
+View.prototype.isEmpty = function() {
+    return !this.$element.length || !this.$element.children().length;
+};
+
+/**
+ * Update current view state (identified by URL).
+ *
+ * @fires View#updateState
+ */
+View.prototype.updateState = function(url) {
+    this.currentURL = url;
+    //this.trigger('updateState', [url]); // XXX not used
+};
+
+/**
+ * @param {(string|Object.<string,*>)} events
+ * @param {*=} selector
+ * @param {*=} data
+ * @param {function(!jQuery.event=)=} handler
+ * @return {View}
+ */
+View.prototype.on = function(events, selector, data, handler) {
+    this.$element.on(events + '.view', selector, data, handler);
+    return this;
+};
+
+View.prototype.one = function(events, selector, data, handler) {
+    this.$element.one(events, selector, data, handler);
+};
+
+/**
+ * @param {(string|Object.<string,*>)} events
+ * @param {*=} selector
+ * @param {*=} data
+ * @param {function(!jQuery.event=)=} handler
+ * @return {View}
+ */
+View.prototype.off = function(events, selector, data, handler) {
+    this.$element.off(events + '.view', selector, data, handler);
+    return this;
+};
+
+/**
+ * @param {(string|jQuery.event)} events
+ * @param {...*} extraParameters
+ * @return {View}
+ */
+View.prototype.trigger = function(events, extraParameters) {
+    this.$element.trigger(events, extraParameters);
+    return this;
+};
+
+View.prototype.show = function() {
+    this.$element.show();
+    if (typeof this.onShow === 'function') {
+        this.onShow.apply(this);
+    }
+    this.trigger('show');
+    return this;
+};
+
+View.prototype.hide = function() {
+    this.$element.hide();
+    if (typeof this.onHide === 'function') {
+        this.onHide.apply(this);
+    }
+    this.trigger('hide');
+    return this;
+};
+
+View.prototype._extractTitle = function(data) {
+    return $($.parseHTML(data)).filter('title').text();
+};
+
+View.prototype._extractAlternateURLs = function(data) {
+    return $($.parseHTML(data)).filter('[rel="alternate"][hreflang]').map(function(i, item){
+        return { lang: $(item).prop('hreflang'), url: $(item).prop('href') };
+    });
+};
+
+/**
+ * Render data to View container. It replaces View content so it is not
+ * required to do View.clean(), but remember: without clean you would louse
+ * some events and also state reset.
+ * XXX: Maybe to do this method private?
+ * @param {String} data Data to be pasted into View container.
+ */
+View.prototype.render = function(data) {
+    //console.debug('[View]:#render');
+    var $data = $($.parseHTML(data));
+    var $selector = $data.find(this.$element.selector);
+    if (!$selector.length) {
+        $selector = $data.filter(this.$element.selector);
+    }
+    var $html = $selector.html();
+    this.$element.html($html);
+    if (typeof this.onRender === 'function') {
+        this.onRender.apply(this);
+    }
+    this.trigger('render');
+    return this;
+};
+
+/**
+ * Updates view content and also changes it state. For now the only state
+ * is supported is current URL.
+ * @param {String} data The html code to be rendered
+ * param {String} url The url to be set for this view. URL will be
+ * reset on View.clean()
+ */
+View.prototype.update = function(data/*, url*/) {
+    this.clean();
+    this.currentTitle = this._extractTitle(data);
+    this.alternateURLs = this._extractAlternateURLs(data);
+    if (arguments[1]) {
+        this.currentURL = arguments[1];
+    } else {
+        this.currentURL = '';
+    }
+
+    this.render(data);
+    this.bind(); // XXX is this used?
+    this.trigger('update', this.currentURL);
+};
+
+View.prototype.bind = function() {
+    // XXX is this used?
+    return this;
+};
+
+View.prototype.unbind = function() {
+    // XXX is this used?
+    return this;
+};
+
+View.prototype.destroy = function() {
+    this.$element.remove();
+    return this;
+};
+
+/**
+ * Method for defining extra cleaning in individual views
+ */
+View.prototype.onBeforeClean = function() {};
+
+/**
+ * Cleans View. It cleans DOM inside view and also resets state (e.g. currentURL)
+ */
+View.prototype.clean = function() {
+    // this.trigger('beforeclean');
+    this.onBeforeClean();
+    this.currentURL = undefined;
+    this.$element.empty();
+    return this;
+};
+
+/**
+ * Scroll view to position, mentioned in `y`.
+ * @param {string|Number} y Position to move to (if integer). To move
+ * down 123px from current place, use '+=123' string (like in jQuery animation)
+ * @param {Number=} animationDuration
+ * param {function} onStep Called on each animation step, with params
+ * `now` and `tween`. See jQuery's scrollTop(options.step) for details
+ * @param {Object=} options
+ * @param {Boolean=} options.force
+ */
+View.prototype.scrollTo = function(y, animationDuration, options) {
+    // XXX: Scroll should be changeable to previous scroll
+
+    console.log('[View]: Scroll to', y, 'with animation', animationDuration);
+    if (this.$container) {
+        var container = this.$container;
+        if (container.get(0) === document.body) {
+            container = $('html,body');
+        }
+        if (typeof window._technicalScroll !== 'number') {
+            window._technicalScroll = 0; // XXX hack
+        }
+        window._technicalScroll += 1;
+        animationDuration = animationDuration ? animationDuration : 0;
+        if (options && options.force) {
+            container.finish();
+        }
+        container.animate(
+            {scrollTop: y},
+            {
+                duration: animationDuration,
+                queue: true,
+                done: function() {
+                    window.setTimeout(function() {  // Hack to run it AFTER onScroll  handlers =(
+                        window._technicalScroll -= 1;
+                    }, 100);
+                }
+
+            }
+        );
+    }
+};
+
